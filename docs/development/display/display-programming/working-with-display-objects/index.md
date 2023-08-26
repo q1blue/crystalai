7e30e59639631831a7c8cd2d27eaa9fee06b4e0c
Crystal_Alchemist <105375894+q1blue@users.noreply.github.com>
2023-08-27 01:39:41 +1000

    Initialize snyk-upgrade-08c9ec3995973896243f0d79bd22763a

---
+++ docs/development/display/display-programming/working-with-display-objects/index.md
@@ -0,0 +1,16 @@
+---
+title: Overview
+sidebar_position: 1
+---
+
+Now that you understand the basic concepts of the Stage, display objects, display object containers, and the display list, this section provides you with some more specific information about working with display objects in ActionScript 3.0.
+
+### More Help topics
+
+- [Properties and methods of the DisplayObject class](properties-and-methods-of-the-displayobject-class.md)
+- [Adding display objects to the display list](adding-display-objects-to-the-display-list.md)
+- [Working with display object containers](working-with-display-object-containers.md)
+- [Traversing the display list](traversing-the-display-list.md)
+- [Setting Stage properties](setting-stage-properties.md)
+- [Handling events for display objects](handling-events-for-display-objects.md)
+- [Choosing a DisplayObject subclass](choosing-a-displayobject-subclass.md)
---
+++ src/components/docs/ASDocsFrame.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {useLocation} from '@docusaurus/router'
+import clsx from 'clsx';
+import styles from './ASDocsFrame.module.css';
+import ExternalLink from '@theme/Icon/ExternalLink';
+import Link from '@docusaurus/Link';
+import StaticFrame from '../general/IFrame';
+
+export default function ASDocsFrame(props) {
+  const { pathname } = useLocation();
+  const components = pathname.split('/');
+  let requestedContent = "/reference/actionscript/3.0";
+  for (var i = components.length-1; i >= 0; --i) {
+    if (components[i].length > 0) {
+      if (components[i] !== 'reference')
+        requestedContent = '/reference/actionscript/' + components[i] ;
+      break;
+    }
+  }
+  return (
+    <>
+      <Link to={'pathname://'+requestedContent} className={clsx(styles.newWindowLink)}>open in new window <ExternalLink/></Link>
+      <StaticFrame src={requestedContent} className={clsx(styles.fullwidthFrame)} />
+    </>
+  );
+}
